9c95c4d5f46907492c81975859a94433579e5dd5
sudongdong <sudongdong@chainup.com>
2020-09-10 17:23:06 +0800

    liquidity url

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -71,7 +71,7 @@ const Header = () => {
                 </Title>
             </HeaderElement>
             <HeaderElement>
-                <Link href="https://pools.balancer.exchange" target="_blank">
+                <Link href="https://nobsu.github.io/balancer-pool.github.io" target="_blank">
                     Add Liquidity
                 </Link>
                 <Wallet />
